b1d955a1ffbd80bc4502b61e509a215ea50dc23b
liu huo <huoliu@gmail.com>
2014-06-19 19:05:26 +0800

    add some basic setup files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='aysha',
+    author='Li Yibo, Liu Huo',
+    version=0.01,
+    url='https://github.com/teou/aysha',
+    packages=['aysha'],
+    description="A very early stage D&D game engine",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+)
