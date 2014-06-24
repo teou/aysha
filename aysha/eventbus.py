015fe338953148e60ec2460b9332c811fc8c3d94
liu huo <huoliu@gmail.com>
2014-06-24 12:01:25 +0800

    add requirements file and propose two methods for event bus decorator

--- aysha/eventbus.py
+++ aysha/eventbus.py
@@ -16,6 +16,8 @@ class EventBus(object):
         if callbacks is None:
             callbacks = {}
             self.subs[event] = callbacks
+
+        #the key and value of the callbacks map are the same, redundency here?
         callbacks[callback] = callback
 
     """
@@ -30,6 +32,14 @@ class EventBus(object):
             return __docall
         return __decorated
     """
+
+    def inner_subscribe(self, event):
+        def __decorated(f):
+            self.sub(event, f)
+            def __replacement(*args, **kwargs):
+                f(*args, **kwargs)
+            return __replacement
+        return __decorated
 
     def unsub_all(self, event=None, callback=None):
         if event is None:
@@ -47,9 +57,24 @@ class EventBus(object):
         callbacks = self.subs.get(event)
         greenlets = []
         if callbacks:
-            for k,v in callbacks.items():
+            for k, v in callbacks.items():
                 greenlets.append(spawn(v, *args, **kwargs))
         return greenlets
+
+
+def outer_subscribe(event_bus, event):
+    def wrap_callback(f):
+        callbacks = event_bus.subs.get(event)
+        if callbacks is None:
+            callbacks = {}
+            event_bus.subs[event] = callbacks
+        callbacks[f] = f
+
+        def wrapped(*args, **kwargs):
+            f(*args, **kwargs)
+        return wrapped
+    return wrap_callback
+
 
 if __name__ == '__main__':
     eb = EventBus()
@@ -64,4 +89,23 @@ if __name__ == '__main__':
     eb.unsub_all(event, testSub)
     greenlets2 = eb.pub(event, "2 haha 2")
     joinall(greenlets2)
+    #######################################
+    eb1 = EventBus()
+    event1 = "event_1"
+
+    @outer_subscribe(eb1, event1)
+    def callback1(test):
+        print 'test callback', test
+
+    greenlets = eb1.pub(event1, "this is argument to callback1")
+    joinall(greenlets)
+    ########################################
+    eb2 = EventBus()
+    event2 = "event_2"
+
+    @eb2.inner_subscribe(event2)
+    def callback2(test):
+        print 'test callback', test
+    greenlets = eb2.pub(event2, "this is argument to callback2")
+    joinall(greenlets)
 
